0196906c5713c7c830afafa84049cbe562552dd0
Vlada2890 <vladashvets11@gmail.com>
2023-01-19 21:32:28 +0100

    messege

---
+++ homework.py
@@ -0,0 +1,16 @@
+#1.16
+def my_function():
+  print("Hello ")
+my_function()
+#2.16
+A = ['a', 'b', 'c', 'd', 'e', '​', '​f']
+def f(A):
+  print(len(A))
+f(A)
+#3.16
+letters = "ABCDEF"
+letters = letters[::-1]
+print(letters)
+
+
+
