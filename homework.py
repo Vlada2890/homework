86be474e25a7dd2372b1a8e2a12c46323c2df14e
Roza <122153166+Vlada2890@users.noreply.github.com>
2023-01-20 20:15:05 +0100

    Update homework.py

--- homework.py
+++ homework.py
@@ -11,6 +11,26 @@ f(A)
 letters = "ABCDEF"
 letters = letters[::-1]
 print(letters)
+#1.17
+a = int(input())
+def t(a):
+    if a % 2 == 0:
+        print("odd number")
+    else:
+      print("even number")
+t(a)
+#2.17
+yhn = input()
+def tt(yhn):
+    if type(yhn) == str:
+      print("This type is str")
+    if type(yhn) == int:
+        print("This type is int")
+tt(yhn)
 
-
-
+#3.17
+n = input()
+c = type(n)
+def yy(n):
+    print('This is', (c))
+yy(n)
